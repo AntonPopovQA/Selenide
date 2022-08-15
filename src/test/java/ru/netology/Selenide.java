9bbd7d856686b8dc0fc2c4bf06eaa260271ef340
Антон Попов <aspirevx74@gmail.com>
2022-08-15 20:59:30 +0300

    DataGenerator

--- src/test/java/ru/netology/Selenide.java
+++ src/test/java/ru/netology/Selenide.java
@@ -1,52 +1,34 @@
 package ru.netology;
 
 import com.codeborne.selenide.Configuration;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-
-
-
 import java.time.Duration;
-
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selenide.*;
 
-public class Selenide {
-    WebDriver driver;
 
-    @BeforeAll
-    static void setUpAll() {
-        WebDriverManager.chromedriver().setup();
-    }
+public class Selenide {
+
 
     @BeforeEach
     void setUp() {
-        driver = new ChromeDriver();
         open("http://localhost:9999");
     }
 
-    @AfterEach
-    void tearDown() {
-        driver.quit();
-    }
 
     @Test
     void shouldTestForm() {
         Configuration.holdBrowserOpen = true;
-
+        String planningDate = DataGenerator.generateDate(3);
         $("[data-test-id=city] input").setValue("Казань");
         $("[data-test-id=date] input").doubleClick();
-        $("[data-test-id=date] input").sendKeys("18.08.2022");
+        $("[data-test-id=date] input").sendKeys(planningDate);
         $("[data-test-id=name] input").setValue("Арсений Орлов-Чесменский");
         $("[data-test-id=phone] input").setValue("+79514555555");
         $("[data-test-id=agreement]").click();
         $x("//*[contains(text(),'Забронировать')]").click();
         $("[data-test-id=notification]")
-                .shouldHave(exactText("Успешно! Встреча успешно забронирована на 18.08.2022"), Duration.ofSeconds(15));
+                .shouldHave(exactText("Успешно! Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15));
     }
 }
